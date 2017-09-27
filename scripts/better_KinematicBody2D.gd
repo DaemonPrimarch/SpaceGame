8a3f0dbfc99c0c242a6cc3130290ce0b286b0ec7
DualMatrix <piet.goris@gmail.com>
2017-09-27 15:44:11 +0200

    Added a value class for collision info, might not work atm though.

--- scripts/better_KinematicBody2D.gd
+++ scripts/better_KinematicBody2D.gd
@@ -21,3 +21,63 @@ func move_no_collision(direction):
 
 func on_collision(body):
 	pass
+
+class KinematicCollision2D:
+	#Collision data for KinematicBody2D collisions.
+	#Copied from future godot version: http://docs.godotengine.org/en/latest/classes/class_kinematiccollision2d.html#class-kinematiccollision2d
+	#Read link for documentation
+	
+	var collider;
+	var collider_id
+	var collider_metadata
+	var collider_shape
+	var collider_shape_index
+	var collider_velocity
+	var local_shape
+	var normal
+	var position
+	var remainder
+	var travel
+	
+	func _init(collider, collider_id, collider_metadata, collider_shape, collider_shape_index, collider_velocity, local_shape, normal, position, remainder, travel):
+		self.collider = collider
+		self.collider_id = collider_id
+		self.collider_metadata = collider_metadata
+		self.collider_shape = collider_shape
+		self.collider_shape_index = collider_shape_index
+		self.collider_velocity = collider_velocity
+		self.local_shape = local_shape
+		self.normal = normal
+		self.position = position
+		self.remainder = remainder
+		self.travel = travel
+	
+	func get_collider():
+		return collider
+	
+	func get_collider_id():
+		return collider_id
+	
+	func get_collider_metadata():
+		return collider_metadata
+	
+	func get_collider_shape():
+		return collider_shape
+		
+	func get_collider_shape_index():
+		return collider_shape_index
+	
+	func get_collider_velocity():
+		return collider_velocity
+	
+	func get_local_shape():
+		return local_shape
+	
+	func get_normal():
+		return normal
+	
+	func get_relainder():
+		return remainder
+		
+	func get_travel():
+		return travel
