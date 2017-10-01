54ed733065f6ea0e92f94ca616cd8d8f27b09eae
DaemonPrimarch <stan.depuydt@student.kuleuven.be>
2017-10-01 12:26:34 +0200

    added iteration_points to beetle (code cleanup needed)

--- nodes/enemies/enemy_beetle.gd
+++ nodes/enemies/enemy_beetle.gd
@@ -2,14 +2,21 @@ extends "res://scripts/enemies/enemy.gd"
 
 # class member variables go here, for example:
 onready var player_detector = get_node("player_detector")
+onready var left_point = get_node("left_turn")
+onready var right_point = get_node("right_turn")
 var player_detected = false
+export var going_right = true
 var move_up = false
 var rot = 0
-var dir = 1
+var dir
 var starting_height
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	if(going_right):
+		dir = 1
+	else:
+		dir = -1
 	connect("collision",self,"on_collision")
 	set_fixed_process(true)
 	starting_height = get_pos()[1]
@@ -23,11 +30,7 @@ func on_collision(info):
 			move_up = false
 			player_detected = false
 		else:
-			dir = -dir
-			if(flippedH):
-				set_flippedH(false)
-			else:
-				set_flippedH(true)
+			turn()
 	elif(info.get_collider().is_in_group("player")):
 		info.get_collider().damage(5)
 		info.get_collider().set_invulnerable(true)
@@ -55,3 +58,13 @@ func _fixed_process(delta):
 			if(rot>=2*PI):
 				rot = 0
 			move(Vector2(1*dir,sin(rot))*get_movement_speed()*delta)
+			if((get_pos()[0] >= right_point.get_pos()[0] and going_right) or (get_pos()[0] <= left_point.get_pos()[0] and !going_right)):
+				turn()
+
+func turn():
+	going_right = !going_right
+	dir = -dir
+	if(flippedH):
+		set_flippedH(false)
+	else:
+		set_flippedH(true)
