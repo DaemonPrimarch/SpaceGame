8bab2244c4b80f19518460704fa914a477a182ed
DualMatrix <piet.goris@gmail.com>
2017-10-17 18:08:24 +0200

    Added area prompt trigger

---
+++ scripts/area_prompt_trigger.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+signal triggered
+
+onready var label = get_node("label")
+
+var bodies_inside = 0
+
+func is_body_inside():
+	return bodies_inside > 0
+	
+
+func _input(event):
+	if(event.is_action_pressed("ui_accept")):
+		emit_signal("triggered")
+		print("TRIGGERED!")
+	
+func _on_body_enter( body ):
+	if(body.is_in_group("player")):
+		bodies_inside += 1
+		set_process_input(true)
+		label.set_text("PRESS BUTTON!")
+	
+
+func _on_body_exit( body ):
+	if(body.is_in_group("player")):
+		bodies_inside -= 1
+		
+		if(bodies_inside <= 0):
+			label.set_text("")
+			set_process(false)
