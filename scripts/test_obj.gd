4f1460c767b07fc129f120e4de9c88e30730d6bb
DualMatrix <piet.goris@gmail.com>
2017-10-18 09:54:20 +0200

    Fixed a minor bug with test_area

--- scripts/test_obj.gd
+++ scripts/test_obj.gd
@@ -9,7 +9,5 @@ func _ready():
 	# Initialization hered
 	pass
 
-
-func _on_area_prompt_trigger_triggered():
-	print("Doing")
+func _on_triggered():
 	dialog_system.play_dialog(dialog_system.create_linear_tree(["Well, I've changed my mind", "I'm letting go.", "Because now I believe in expansion.", "I believe we endure", "Don't you see?", "Everything lives!"]))
