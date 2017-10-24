1fff2ed1dc4773299447d72e7610ba807c326a2a
DualMatrix <piet.goris@gmail.com>
2017-10-24 18:00:44 +0200

    Made warp tiles way easier to use

--- scripts/warp_tile.gd
+++ scripts/warp_tile.gd
@@ -2,16 +2,16 @@ extends Node2D
 
 var loaded = false
 export var active = true setget set_active, is_active
-export var destination_scene_path = "res://nodes/rooms/"
 export var arrival_node_ID = "arrival_name"
+export(PackedScene) var destination_scene
 
 func on_enter(body):
 	if(body.is_in_group("warpable") and not loaded):
-		if(destination_scene_path == "res://nodes/rooms/"):
+		if(destination_scene == null):
 			print("door not loaded")
 		else:
-			room_loader.call_deferred("warp_player_to_new_room", body, destination_scene_path, arrival_node_ID)
-
+			room_loader.call_deferred("warp_player_to_new_room", body, destination_scene.get_path(), arrival_node_ID)
+			
 func is_active():
 	return active
 
