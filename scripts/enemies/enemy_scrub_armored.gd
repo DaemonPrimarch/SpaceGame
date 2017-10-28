9d56edeef8f86b1a04f4f30a46d34af12af26245
DaemonPrimarch <stan.depuydt@student.kuleuven.be>
2017-10-28 13:32:29 +0200

    added room 8 and 9

---
+++ scripts/enemies/enemy_scrub_armored.gd
@@ -0,0 +1,10 @@
+extends "res://scripts/enemies/enemy_scrub.gd"
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
