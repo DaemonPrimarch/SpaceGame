7c8313df9abb6079d60145892ce3b8e16062c317
DaemonPrimarch <stan.depuydt@student.kuleuven.be>
2017-10-29 11:53:42 +0100

    added room 13 and 14

--- scripts/timed_door.gd
+++ scripts/timed_door.gd
@@ -3,20 +3,24 @@ extends StaticBody2D
 onready var timer_open = get_node("Timer_open")
 onready var timer_closed = get_node("Timer_closed")
 
+func hide(value):
+	set_hidden(value)
+	set_layer_mask_bit(1, not value)
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	set_hidden(true)
+	hide(true)
 	timer_open.start()
 
 
 func _on_Timer_open_timeout():
-	set_hidden(not is_hidden())
+	hide(not is_hidden())
 	timer_closed.start()
 	timer_open.stop()
 
 
 func _on_Timer_closed_timeout():
-	set_hidden(not is_hidden())
+	hide(not is_hidden())
 	timer_closed.stop()
 	timer_open.start()
