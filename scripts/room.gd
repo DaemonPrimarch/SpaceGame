b5fad7daa3b25b3b278762b07a0080012702db23
DualMatrix <piet.goris@gmail.com>
2017-10-30 14:51:42 +0100

    Added test_player support, better this time

--- scripts/room.gd
+++ scripts/room.gd
@@ -7,10 +7,7 @@ var loaded
 export var camera_zoom = Vector2(1,1)
 
 func _ready():
-	get_node("test_player").set_fixed_process(false)
-	get_node("test_player").set_process_input(false)
-	get_node("test_player").set_process(false)
-	get_node("test_player").set_hidden(true)
+	pass
 
 func get_camera_zoom():
 	return camera_zoom
@@ -19,15 +16,12 @@ func set_player_zoom(player):
 	player.get_node("camera").set_zoom(get_camera_zoom())
 
 func _on_test_player_existance_timer_timeout():
-	if(loaded):
-		get_node("test_player").queue_free()
-	else:
-		get_node("test_player").set_fixed_process(true)
-		get_node("test_player").set_process_input(true)
-		get_node("test_player").set_process(true)
-		get_node("test_player").set_hidden(false)
-		emit_signal("player_enter", get_node("test_player"))
-
+	if(not loaded):
+		var new_player = preload("res://nodes/player.tscn").instance()
+		new_player.set_pos(get_node("test_player_spawn_point").get_pos())
+		add_child(new_player)
+		new_player.set_name("test_player")
+		emit_signal("player_enter", new_player)
 
 func _on_room_player_enter(player):
 	loaded = true
