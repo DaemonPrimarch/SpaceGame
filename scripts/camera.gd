6703415f7cf997d212a212e37210e835481c6d94
DualMatrix <piet.goris@gmail.com>
2017-11-06 20:55:23 +0100

    Fixed camera not working with multiple terrains

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -12,24 +12,29 @@ func update_limit():
 		if(terrains.size() >= 1):
 			var posX = []
 			var posY = []
+			var m_posX = []
+			var m_posY = []
 			var terrain_found = false
 			var total_size = Vector2()
 			
 			for terrain in terrains:
 				if(terrain extends TileMap):
 					terrain_found = true
-					posX.append((terrain.get_used_rect().pos.x*terrain.get_cell_size().x + terrain.get_pos().x))
-					posY.append((terrain.get_used_rect().pos.y*terrain.get_cell_size().y + terrain.get_pos().y))
-					total_size += (terrain.get_used_rect().size * terrain.get_cell_size())
-				
+					
+					print(terrain.get_used_rect())
+					
+					posX.append((terrain.get_used_rect().pos.x*terrain.get_cell_size().x + terrain.get_global_pos().x))
+					posY.append((terrain.get_used_rect().pos.y*terrain.get_cell_size().y + terrain.get_global_pos().y))
+					m_posX.append((terrain.get_used_rect().pos.x*terrain.get_cell_size().x + terrain.get_global_pos().x + terrain.get_used_rect().size.x * terrain.get_cell_size().x))
+					m_posY.append((terrain.get_used_rect().pos.y*terrain.get_cell_size().y + terrain.get_global_pos().y + terrain.get_used_rect().size.y * terrain.get_cell_size().y))
 			if(terrain_found):
 				var min_x = min_arr(posX)
 				var min_y = min_arr(posY)
 				
 				set_limit(0, min_x)
 				set_limit(1, min_y)
-				set_limit(2, min_x + total_size.x)
-				set_limit(3, min_y + total_size.y)
+				set_limit(2, max_arr(m_posX))
+				set_limit(3, max_arr(m_posY))
 
 func min_arr(arr):
     var min_val = arr[0]
@@ -39,5 +44,11 @@ func min_arr(arr):
 
     return min_val
 	
-	
+func max_arr(arr):
+    var min_val = arr[0]
+
+    for i in range(1, arr.size()):
+        min_val = max(min_val, arr[i])
+
+    return min_val
 	
