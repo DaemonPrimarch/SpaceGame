a9eda981f5531581c7181c832e7f862a283aee18
DualMatrix <piet.goris@gmail.com>
2018-02-27 16:57:03 +0100

    Reintroduced slopes

--- scripts/extended_kinematic_body_2D.gd
+++ scripts/extended_kinematic_body_2D.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 signal collided(info, moved_object)
 
-export var max_slope_angle = PI/2
+export var max_slope_angle = PI/4
 
 func get_max_slope_angle():
 	return max_slope_angle
@@ -19,12 +19,16 @@ func move_and_collide(v):
 	
 func move_and_collide_slope(v):
 	var collision_info = move_and_collide(v)
+	
 
-	while(collision_info != null and abs(collision_info.normal.rotated(PI/2).angle()) < max_slope_angle and abs(collision_info.normal.rotated(PI/2).angle()) > 0):
-		if(has_node("debug_vector")):
-			get_node("debug_vector").set_cast_to(collision_info.remainder.dot(collision_info.normal.rotated(PI/2)) * collision_info.normal.rotated(PI/2) * 500)
-			
-		collision_info = move_and_collide(collision_info.remainder.dot(collision_info.normal.rotated(PI/2)) * collision_info.normal.rotated(PI/2))
+	if(collision_info != null and abs(collision_info.normal.rotated(PI/2).angle()) < max_slope_angle and abs(collision_info.normal.rotated(PI/2).angle()) > 0):
+		if(has_node("debuggyboy")):
+			#get_node("debuggyboy").set_cast_to(collision_info.remainder.dot(collision_info.normal.rotated(PI/2)) * collision_info.normal.rotated(PI/2) * 10000)
+			get_node("debuggyboy").set_cast_to(collision_info.normal.rotated(PI/2) * 10000)
+		
+		print(collision_info.normal.rotated(PI/2).angle())
+		
+		collision_info = move_and_collide(collision_info.remainder.dot(collision_info.normal.rotated(PI/2)) * collision_info.normal.rotated(PI/2)) 
 		
 		
 	return collision_info
