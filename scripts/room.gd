c4eae82e85d35bb8aa6e76c3d58dd658060d5ea2
DualMatrix <piet.goris@gmail.com>
2018-03-15 01:57:21 +0100

    Moving towards better camera

--- scripts/room.gd
+++ scripts/room.gd
@@ -8,9 +8,11 @@ signal room_darkened
 
 export var dark = true setget set_dark, is_dark 
 export var dark_color = Color(0,0,0)
+export var auto_generate_camera_container = true
 
 func _ready():
-	pass
+	if(auto_generate_camera_container):
+		generate_camera_container()
 
 func set_dark(val):
 	dark = val
@@ -28,3 +30,22 @@ func _on_room_room_darkened():
 
 func _on_room_room_lit():
 	get_node("CanvasModulate").color = Color(1,1,1)
+
+func generate_camera_container():
+	var min_point = Vector2(10000, 10000)
+	
+	for tile in get_node("terrain").get_used_cells():
+		if(tile.x < min_point.x):
+			min_point = tile
+	
+	var scrolling_point = min_point
+	var polygon = []
+	var terrain = get_node("terrain")
+	var rect = terrain.get_used_rect()
+	
+	polygon.push_back(rect.position * terrain.cell_size)
+	polygon.push_back((rect.position + rect.size * Vector2(0, 1)) * terrain.cell_size)
+	polygon.push_back((rect.position + rect.size) * terrain.cell_size)
+	polygon.push_back((rect.position + rect.size * Vector2(1, 0)) * terrain.cell_size)
+	
+	get_node("camera_container").polygon = polygon
