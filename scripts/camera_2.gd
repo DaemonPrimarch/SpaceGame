66b55dbae0cbbfac9d618b23e42efb0629d6d95d
DualMatrix <piet.goris@gmail.com>
2018-03-17 17:02:22 +0100

    Advanced zoom

--- scripts/camera_2.gd
+++ scripts/camera_2.gd
@@ -11,27 +11,37 @@ export var zoom = 1.0 setget set_zoom, get_zoom
 export var stay_within_terrain = true
 
 signal completed_zoom
+signal entered_container
+
+var container = null
+
+func is_in_container():
+	return container != null
+	
+func set_container(cont):
+	container = cont
+	
+	set_zoom(1)
+	
+func get_container():
+	return container
+
+func get_follow_point():
+	return get_parent().global_position
 
 func _ready():
 	default_pos = position
-	
-	get_parent().connect("room_entered", self, "on_room_enter")
-
-func on_room_enter():
-	
-	if(get_node("/root/ROOM_MANAGER").get_room_of_node(self).has_default_camera_position()):
-		default_pos = get_node("/root/ROOM_MANAGER").get_room_of_node(self).get_default_camera_position()
 
 func set_zoom(new_zoom):	
 	call_deferred("scale_viewport", (Vector2(new_zoom/get_zoom(), new_zoom/get_zoom())))
-	
-		
+
 	zoom = new_zoom
 
 func zoom_to(zoom, time):
-	if(not get_node("zoom_to").is_active()):
-		get_node("zoom_to").interpolate_method (self, "set_zoom", get_zoom(), zoom, time, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN, 0 )
-		get_node("zoom_to").start()
+	print("ZOOMING TO:  ", zoom)
+	get_node("zoom_to").stop_all()
+	get_node("zoom_to").interpolate_method (self, "set_zoom", get_zoom(), zoom, time, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN, 0 )
+	get_node("zoom_to").start()
 	
 func scale_viewport(scale):
 	if(is_inside_tree()):
@@ -45,6 +55,9 @@ func scale_viewport(scale):
 func get_zoom():
 	return zoom
 
+func _exit_tree():
+	set_container(null)
+
 func _physics_process(delta):
 #	if(Input.is_action_pressed("play_up")):
 #		position += Vector2(0, -64 * 2) * delta
@@ -54,85 +67,80 @@ func _physics_process(delta):
 #		position += Vector2(-64 * 2, 0) * delta
 #	if(Input.is_action_pressed("play_right")):
 #		position += Vector2(64 * 2, 0) * delta
+
 	if(active):
-		if(stay_within_terrain):
-			var dir = Vector2()
+		var dir = Vector2()
 			
-			if((default_pos - position).length() <= 64):
-				dir = default_pos - position
-			else:
-				dir = (default_pos - position).normalized() * 64
+		if((default_pos - position).length() <= 64):
+			dir = default_pos - position
+		else:
+			dir = (default_pos - position).normalized() * 64
+		
+		get_node("debug/default_pos").position = -position + default_pos
 			
-			get_node("debug/default_pos").position = -position + default_pos
+		get_node("debug/RayCast2D").cast_to = dir
 			
-			get_node("debug/RayCast2D").cast_to = dir
+		position += dir
 			
-			position += dir
+		get_node("debug/Sprite").modulate = Color(255, 0, 0)
+		get_node("debug/tr").modulate = Color(255, 0, 0)
+		get_node("debug/br").modulate = Color(255, 0, 0)
+		get_node("debug/tl").modulate = Color(255, 0, 0)
+		get_node("debug/bl").modulate = Color(255, 0, 0)
 			
-			get_node("debug/Sprite").modulate = Color(255, 0, 0)
-			get_node("debug/tr").modulate = Color(255, 0, 0)
-			get_node("debug/br").modulate = Color(255, 0, 0)
-			get_node("debug/tl").modulate = Color(255, 0, 0)
-			get_node("debug/bl").modulate = Color(255, 0, 0)
+		var bottom_right = get_viewport().size/2 / PHYSICS_HELPER.get_global_scale_of_node(self) / zoom
+		var bottom_left = get_viewport().size/2 * Vector2(-1, 1) / PHYSICS_HELPER.get_global_scale_of_node(self) / zoom
+		var top_right = get_viewport().size/2 * Vector2(1, -1) / PHYSICS_HELPER.get_global_scale_of_node(self) / zoom
+		var top_left = get_viewport().size/2 * -1 / PHYSICS_HELPER.get_global_scale_of_node(self) / zoom
 			
-			var bottom_right = get_viewport().size/2 / PHYSICS_HELPER.get_global_scale_of_node(self) / zoom
-			var bottom_left = get_viewport().size/2 * Vector2(-1, 1) / PHYSICS_HELPER.get_global_scale_of_node(self) / zoom
-			var top_right = get_viewport().size/2 * Vector2(1, -1) / PHYSICS_HELPER.get_global_scale_of_node(self) / zoom
-			var top_left = get_viewport().size/2 * -1 / PHYSICS_HELPER.get_global_scale_of_node(self) / zoom
-			
-			get_node("debug/tr").position = top_right
-			get_node("debug/br").position = bottom_right
-			get_node("debug/tl").position = top_left
-			get_node("debug/bl").position = bottom_left
-			
-			for pol in get_tree().get_nodes_in_group("camera_container"):
+		get_node("debug/tr").position = top_right
+		get_node("debug/br").position = bottom_right
+		get_node("debug/tl").position = top_left
+		get_node("debug/bl").position = bottom_left	
+		
+		if(stay_within_terrain and is_in_container()):
+			var offset = Vector2()
 				
+			var polygon  = get_container().get_global_polygon()
 				
-				var offset = Vector2()
-				
-				var polygon  = []
-				
-				for p in pol.polygon:
-					polygon.push_back(p + pol.global_position)
-				
-				if(MATHS.is_point_in_polygon(get_parent().global_position, polygon)):
-					get_node("debug/Sprite").modulate = Color(0, 255, 0)
-					if(not MATHS.is_point_in_polygon(to_global(top_left), polygon)):
-						get_node("debug/tl").modulate = Color(0, 255, 0)
-						
-						var new_offset = MATHS.offset_point_polygon(to_global(top_left), polygon)
-						
-						if(abs(offset.x) < abs(new_offset.x)):
-							offset.x = new_offset.x
-						if(abs(offset.y) < abs(new_offset.y)):
-							offset.y = new_offset.y
-					if(not MATHS.is_point_in_polygon(to_global(top_right), polygon)):
-						get_node("debug/tr").modulate = Color(0, 255, 0)
-						
-						var new_offset = MATHS.offset_point_polygon(to_global(top_right), polygon)
-						
-						if(abs(offset.x) < abs(new_offset.x)):
-							offset.x = new_offset.x
-						if(abs(offset.y) < abs(new_offset.y)):
-							offset.y = new_offset.y
-					if(not MATHS.is_point_in_polygon(to_global(bottom_left), polygon)):
-						get_node("debug/bl").modulate = Color(0, 255, 0)
-						
-						var new_offset = MATHS.offset_point_polygon(to_global(bottom_left), polygon)
-						
-						if(abs(offset.x) < abs(new_offset.x)):
-							offset.x = new_offset.x
-						if(abs(offset.y) < abs(new_offset.y)):
-							offset.y = new_offset.y
-					if(not MATHS.is_point_in_polygon(to_global(bottom_right),polygon)):
-						get_node("debug/br").modulate = Color(0, 255, 0)
-						var new_offset = MATHS.offset_point_polygon(to_global(bottom_right), polygon)
-						
-						if(abs(offset.x) < abs(new_offset.x)):
-							offset.x = new_offset.x
-						if(abs(offset.y) < abs(new_offset.y)):
-							offset.y = new_offset.y	
-					position -= offset
+			if(MATHS.is_point_in_polygon(get_parent().global_position, polygon)):
+				get_node("debug/Sprite").modulate = Color(0, 255, 0)
+				if(not MATHS.is_point_in_polygon(to_global(top_left), polygon)):
+					get_node("debug/tl").modulate = Color(0, 255, 0)
 					
-		get_viewport().canvas_transform.origin = (-global_position  * get_viewport().canvas_transform.get_scale()) + get_viewport_rect().size/2
+					var new_offset = MATHS.offset_point_polygon(to_global(top_left), polygon)
+					
+					if(abs(offset.x) < abs(new_offset.x)):
+						offset.x = new_offset.x
+					if(abs(offset.y) < abs(new_offset.y)):
+						offset.y = new_offset.y
+				if(not MATHS.is_point_in_polygon(to_global(top_right), polygon)):
+					get_node("debug/tr").modulate = Color(0, 255, 0)
+					
+					var new_offset = MATHS.offset_point_polygon(to_global(top_right), polygon)
+					
+					if(abs(offset.x) < abs(new_offset.x)):
+						offset.x = new_offset.x
+					if(abs(offset.y) < abs(new_offset.y)):
+						offset.y = new_offset.y
+				if(not MATHS.is_point_in_polygon(to_global(bottom_left), polygon)):
+					get_node("debug/bl").modulate = Color(0, 255, 0)
+					
+					var new_offset = MATHS.offset_point_polygon(to_global(bottom_left), polygon)
+					
+					if(abs(offset.x) < abs(new_offset.x)):
+						offset.x = new_offset.x
+					if(abs(offset.y) < abs(new_offset.y)):
+						offset.y = new_offset.y
+				if(not MATHS.is_point_in_polygon(to_global(bottom_right),polygon)):
+					get_node("debug/br").modulate = Color(0, 255, 0)
+					var new_offset = MATHS.offset_point_polygon(to_global(bottom_right), polygon)
+					
+					if(abs(offset.x) < abs(new_offset.x)):
+						offset.x = new_offset.x
+					if(abs(offset.y) < abs(new_offset.y)):
+						offset.y = new_offset.y	
+				position -= offset
+					
+		get_viewport().canvas_transform.origin = ((-global_position - position + position * PHYSICS_HELPER.get_global_scale_of_node(self))  * get_viewport().canvas_transform.get_scale()) + get_viewport_rect().size/2
 		
--- scripts/camera_zoom_out_manager.gd
+++ scripts/camera_zoom_out_manager.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var zoomed = false
+
 func _ready():
 	get_parent().connect("state_entered", self, "on_state_entered")
 	get_parent().connect("state_left", self, "on_state_left")
@@ -11,14 +13,17 @@ func on_state_entered(state, old_state):
 func on_state_left(state, new_state):
 	if(state == "STANDING"):
 		get_node("Timer").stop()
+		if(zoomed):
+			print("Zoomoon")
+			get_parent().get_node("camera2").zoom_to(1, 0.5)
+			zoomed = false
 
 func _on_Timer_timeout():
-	var size = get_node("/root/MATHS").get_aabb_of_polygon(get_node("/root/ROOM_MANAGER").get_room_of_node(self).get_node("camera_container").polygon).size / get_viewport_rect().size
+	var size = get_node("/root/MATHS").get_aabb_of_polygon(get_parent().get_node("camera2").get_container().polygon).size / get_viewport_rect().size
 	
 	var target = 1.0/min(size.x, size.y)
 	
-	print("ZOOMING OUT TO: ", target) 
-	print(size)
-	
 	get_parent().get_node("camera2").zoom_to(target, 4)
+	
+	zoomed = true
 	
