6b765d0ee8dba5e1b7939689a4d182f17868ce3c
DualMatrix <piet.goris@gmail.com>
2018-03-19 22:58:59 +0100

    Made transistion between containers clean

--- scripts/camera_2.gd
+++ scripts/camera_2.gd
@@ -5,6 +5,7 @@ export var active = true
 var offset = Vector2()
 
 var original_size = Vector2()
+export var velocity = Vector2(64, 64) 
 
 export var zoom = 1.0 setget set_zoom, get_zoom
 
@@ -64,8 +65,17 @@ func set_container(cont):
 		if(cont.has_default_camera_offset()):
 			move_offset_to(cont.get_default_camera_offset(), 0.2)
 		if(old_cont):
-			temp_containers = [old_cont]
+			var bottom_right = get_viewport().size/2  / zoom
+			var bottom_left = get_viewport().size/2 * Vector2(-1, 1) / zoom
+			var top_right = get_viewport().size/2 * Vector2(1, -1)  / zoom
+			var top_left = get_viewport().size/2 * -1 / zoom
 
+			position -= MATHS.offset_rect_polygon(Rect2(to_global(top_left), bottom_right - top_left), old_cont.get_global_polygon())
+
+			set_control_mode(DIRECT_CONTROL)
+		
+			move_to(position - MATHS.offset_rect_polygon(Rect2(to_global(top_left), bottom_right - top_left), cont.get_global_polygon()), 0.5)
+	
 func _on_camer_transfer_timer_timeout():
 	temp_containers = []
 	moving = false
@@ -126,7 +136,6 @@ func move_offset_to(to, time):
 
 var moving = false
 func move_to(pos, time):
-	print("MOVe")
 	moving  = true
 	get_node("camer_transfer_timer").start()
 	
@@ -134,16 +143,12 @@ func move_to(pos, time):
 	get_node("move_to").interpolate_property (self, "position", position, pos, time, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN, 0 )
 	get_node("move_to").start()
 
-var time = 0
+func is_moving():
+	return moving
 
 func _physics_process(delta):
 	if(is_active()):	
 		get_node("debug").visible = debug_labels_enabled()
-		
-		var bottom_right = get_viewport().size/2  / zoom
-		var bottom_left = get_viewport().size/2 * Vector2(-1, 1) / zoom
-		var top_right = get_viewport().size/2 * Vector2(1, -1)  / zoom
-		var top_left = get_viewport().size/2 * -1 / zoom		
 		
 		if(debug_labels_enabled()):	
 			get_node("debug/Sprite").modulate = Color(255, 0, 0)
@@ -151,11 +156,6 @@ func _physics_process(delta):
 			get_node("debug/br").modulate = Color(255, 0, 0)
 			get_node("debug/tl").modulate = Color(255, 0, 0)
 			get_node("debug/bl").modulate = Color(255, 0, 0)
-			
-			get_node("debug/tr").position = top_right
-			get_node("debug/br").position = bottom_right
-			get_node("debug/tl").position = top_left
-			get_node("debug/bl").position = bottom_left	
 		
 		if(get_control_mode() == FOLLOW_PARENT):
 			var dir = Vector2()
@@ -171,8 +171,9 @@ func _physics_process(delta):
 				get_node("debug/RayCast2D").cast_to = dir
 				
 			position += dir
+
 		elif(get_control_mode() == DIRECT_CONTROL):
-			if(debug_controls_enabled()):
+			if(debug_controls_enabled() and not true):
 				var speed = 64 * 4
 				
 				if(Input.is_action_pressed("play_up")):
@@ -185,76 +186,16 @@ func _physics_process(delta):
 					position += Vector2(speed, 0) * delta
 		
 			position -= (get_parent().global_position - fixed_position)
-		if(stay_within_terrain and is_in_container()):
-			offset = Vector2()
-				
-			var polygon  = get_container().get_global_polygon()
-				
-			if(MATHS.is_point_in_polygon(get_parent().global_position, polygon)):
-				get_node("debug/Sprite").modulate = Color(0, 255, 0)
-				if(not MATHS.is_point_in_polygon(to_global(top_left), polygon)):
-					get_node("debug/tl").modulate = Color(0, 255, 0)
-					
-					var new_offset = MATHS.offset_point_polygon(to_global(top_left), polygon)
-					
-					if(abs(offset.x) < abs(new_offset.x)):
-						offset.x = new_offset.x
-					if(abs(offset.y) < abs(new_offset.y)):
-						offset.y = new_offset.y
-				if(not MATHS.is_point_in_polygon(to_global(top_right), polygon)):
-					get_node("debug/tr").modulate = Color(0, 255, 0)
-					
-					var new_offset = MATHS.offset_point_polygon(to_global(top_right), polygon)
-					
-					if(abs(offset.x) < abs(new_offset.x)):
-						offset.x = new_offset.x
-					if(abs(offset.y) < abs(new_offset.y)):
-						offset.y = new_offset.y
-				if(not MATHS.is_point_in_polygon(to_global(bottom_left), polygon)):
-					get_node("debug/bl").modulate = Color(0, 255, 0)
-					
-					var new_offset = MATHS.offset_point_polygon(to_global(bottom_left), polygon)
-					
-					if(abs(offset.x) < abs(new_offset.x)):
-						offset.x = new_offset.x
-					if(abs(offset.y) < abs(new_offset.y)):
-						offset.y = new_offset.y
-				if(not MATHS.is_point_in_polygon(to_global(bottom_right),polygon)):
-					get_node("debug/br").modulate = Color(0, 255, 0)
-					var new_offset = MATHS.offset_point_polygon(to_global(bottom_right), polygon)
-					
-					if(abs(offset.x) < abs(new_offset.x)):
-						offset.x = new_offset.x
-					if(abs(offset.y) < abs(new_offset.y)):
-						offset.y = new_offset.y	
-				
-				var fancy_offset = offset
-				
-				offset = Vector2()
-				
-				for container in temp_containers:
-					polygon  = container.get_global_polygon()
-					
-					offset = MATHS.offset_rect_polygon(Rect2(to_global(top_left), bottom_right - top_left), polygon)
-				
-				if(temp_containers.size()):
-					if(not moving):
-						
-						var old_pos = position
-						position -= offset
-						
-						print(offset, fancy_offset)
-						
-						get_parent().get_node("start").position = position
-						get_parent().get_node("stop").position = (position - fancy_offset)
-						#get_parent().set_gravity_enabled(false)
-						
-						set_control_mode(DIRECT_CONTROL)
-						
-						move_to(old_pos - fancy_offset, 0.5)
-						print("MOVING TO: ", fancy_offset)
-				else:
-					position -= fancy_offset
 		
+		var bottom_right = get_viewport().size/2  / zoom
+		var bottom_left = get_viewport().size/2 * Vector2(-1, 1) / zoom
+		var top_right = get_viewport().size/2 * Vector2(1, -1)  / zoom
+		var top_left = get_viewport().size/2 * -1 / zoom		
+		get_node("debug/tr").position = top_right
+		get_node("debug/br").position = bottom_right
+		get_node("debug/tl").position = top_left
+		get_node("debug/bl").position = bottom_left	
+		if(is_staying_within_terrain() and is_in_container() and not is_moving()):
+			position -= MATHS.offset_rect_polygon(Rect2(to_global(top_left), bottom_right - top_left), get_container().get_global_polygon())
 		get_viewport().canvas_transform.origin = ((-global_position - position + position * PHYSICS_HELPER.get_global_scale_of_node(self))  * get_viewport().canvas_transform.get_scale()) + get_viewport_rect().size/2
 
