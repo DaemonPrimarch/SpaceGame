f1a3535ff8a082e0d9b6170654703eb5ce0ff44c
DaemonPrimarch <stan.depuydt@student.kuleuven.be>
2018-03-25 00:26:45 +0100

    updated room 9 to_engine

--- scripts/trigger_area.gd
+++ scripts/trigger_area.gd
@@ -1,6 +1,19 @@
 extends Area2D
 
-func _on_area_enter(body, key):
+export var one_time = false;
+var player
+
+signal player_entered
+signal platform_entered
+
+func _on_area_enter(body):
 	if(body.is_in_group("player")):
-		body.add_to_inventory(key)
-	queue_free()
+		player = body
+		emit_signal("player_entered")
+	if(body.is_in_group("platform")):
+		emit_signal("platform_entered")
+
+func give_key(key):
+	player.add_to_inventory(key)
+	if(one_time):
+		queue_free()
