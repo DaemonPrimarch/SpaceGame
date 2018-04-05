48eee9c254dc6f5a797690de3bcf1e03a00e8217
DaemonPrimarch <stan.depuydt@student.kuleuven.be>
2018-04-05 17:42:42 +0200

    enemy_2 small fix

--- scripts/state_handlers/enemies/stunned_by_exhaustion_handler.gd
+++ scripts/state_handlers/enemies/stunned_by_exhaustion_handler.gd
@@ -5,7 +5,6 @@ func get_handled_state():
 	
 func enter_state(previous_state):
 	.enter_state(previous_state)
-	get_parent().get_node("eye_white").set("modulate", Color(0,0,0))
 	
 func leave_state(new_state):
 	.leave_state(new_state)
