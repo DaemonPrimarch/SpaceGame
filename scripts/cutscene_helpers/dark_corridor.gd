e9f7f79aa5a7ee07a83b7f43c0fd6f976f93df78
DualMatrix <piet.goris@gmail.com>
2018-04-26 22:57:02 +0200

    More cutscene work

--- scripts/cutscene_helpers/dark_corridor.gd
+++ scripts/cutscene_helpers/dark_corridor.gd
@@ -4,10 +4,10 @@ func _physics_process(delta):
 	var player = ROOM_MANAGER.get_room_of_node(self).get_node("player")
 	var puppet = get_node("../puppet_1")
 
-	var min_speed = 164
+	var min_speed = 162
 	
 	if(player.global_position.x > puppet.global_position.x + 64 +32):
 		print(player.get_max_movement_speed())
-		player.set_movement_speed(min(164 + ( player.global_position.x - puppet.global_position.x)/2, player.get_max_movement_speed()))
+		player.set_movement_speed(min(162 + ( player.global_position.x - puppet.global_position.x)/2, player.get_max_movement_speed()))
 	else:
-		player.set_movement_speed(164)
+		player.set_movement_speed(162)
--- scripts/room_elements/on_enter_cutscene_player.gd
+++ scripts/room_elements/on_enter_cutscene_player.gd
@@ -2,7 +2,14 @@ extends Node2D
 
 func _on_enter_trigger_player_entered(player):
 	if(has_node("cutscene_player")):
-		print("PLAYYYYYYYY")
 		get_node("cutscene_player").play_cutscene()
+		get_node("cutscene_player").connect("animation_finished", self, "cutscene_finished")
 	else:
 		print("Cutscene not found!")
+
+func cutscene_finished():
+	queue_free()
+
+func _on_enter_trigger_tree_exited():
+	if(not get_node("cutscene_player").is_playing()):
+		queue_free()
