e14bb0d2ace79f26ad053f8e1df497cd1cd51031
DualMatrix <piet.goris@gmail.com>
2018-06-03 00:44:05 +0200

    More cleanup

--- scripts/room_elements/game_saver.gd
+++ scripts/room_elements/game_saver.gd
@@ -7,5 +7,4 @@ func save_and_recharge(player):
 	SAVE_MANAGER.save_current_file()
 
 func save_and_recharge_np():
-	print("SAVVV")
 	save_and_recharge(get_node("/root/ROOM_MANAGER").get_room_of_node(self).get_node("player"))
