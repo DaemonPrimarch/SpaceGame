d0a363016d822b437d4370c506efb626f40b50b3
DualMatrix <piet.goris@gmail.com>
2018-07-03 11:44:03 +0200

    More fixes

--- addons/state_labels/debug_label.gd
+++ addons/state_labels/debug_label.gd
@@ -1,8 +1,4 @@
 extends Label
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	visible = ProjectSettings.get_setting("debug/settings/debug_labels_enabled")
