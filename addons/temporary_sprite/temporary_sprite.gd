aed12b81ba029434d2fad80daab4efd4bf45bfae
DualMatrix <piet.goris@gmail.com>
2018-07-09 01:38:51 +0200

    Added Temporary Sprites to some nodes

--- addons/temporary_sprite/temporary_sprite.gd
+++ addons/temporary_sprite/temporary_sprite.gd
@@ -8,8 +8,11 @@ func _ready():
 		label.name = "NameLabel"
 		label.text = "Name"
 		
+		label.rect_position = Vector2(2, 24)
+		
 		label.align = Label.ALIGN_CENTER
-			
+		label.valign = Label.VALIGN_CENTER
+		
 		add_child(label)
 		
 		label.set_owner(get_tree().get_edited_scene_root())
