94e53b2194e76d557bc5da71c4daa99208f15120
DualMatrix <piet.goris@gmail.com>
2018-07-10 02:22:58 +0200

    Fixes to shadow monster

--- scripts/entities/enemies/shadow_monster.gd
+++ scripts/entities/enemies/shadow_monster.gd
@@ -2,14 +2,12 @@ tool
 
 extends "res://scripts/entities/enemies/enemy.gd"
 
-export var attack_duration = 0.2
 export var eyes_visible= true setget set_eyes_visible
-
-var attacking = false
 
 func _init():
 	add_state("BODY_INVISIBLE")
 	add_state("SILLOUETTE_VISIBLE")
+	add_state("SILLOUETTE_ATTACK")
 	add_state("FULL_VISIBLE")
 	
 func set_eyes_visible(val):
@@ -22,13 +20,9 @@ func set_eyes_visible(val):
 func get_player():
 	return get_parent().get_node("player")
 
-func attack_player():	
-	var play = get_player()
-	attacking = true
-	
-	if(play):
-		$attack.interpolate_property(self, "position", position, play.position, attack_duration, Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
-		$attack.start()
+func attack_player(player):
+	get_handler("SILLOUETTE_ATTACK").set_player_attacking(player)
+	set_state("SILLOUETTE_ATTACK")
 	
 func _on_shadow_monster_light_entered(source):
 #	if(not attacking):
