42a5035b01285abf69e3d31d80ad871525259cbe
DualMatrix <piet.goris@gmail.com>
2018-07-12 17:11:39 +0200

    Fixed room_12 and player light

--- scripts/player_light.gd
+++ scripts/player_light.gd
@@ -1,10 +1,2 @@
 extends Light2D
 
-func _ready():
-	get_parent().connect("room_entered", self, "_on_player_room_entered")
-
-func _on_player_room_entered():
-	if(get_node("/root/ROOM_MANAGER").get_room_of_node(self).is_dark()):
-		enabled = true
-	else:
-		enabled = false
