b4b98b566c081716f7e9aba40ef1577284212cc9
DualMatrix <piet.goris@gmail.com>
2018-08-07 17:14:01 +0200

    Fixes to camera's

--- scripts/helpers/respawn_manager.gd
+++ scripts/helpers/respawn_manager.gd
@@ -20,5 +20,5 @@ func _physics_process(delta):
 	get_node("ground_detector").force_raycast_update()
 	
 	if(not get_parent().is_inside_helper_area("no_respawn") and get_parent().is_grounded() and get_node("ground_detector").is_colliding() and get_node("ground_detector").get_collider().is_in_group("terrain")):
-		last_safe_position = get_parent().position
+		last_safe_position = get_parent().global_position
 		
