a7fd9553b0178ad8ff0b3141653444f77f44c24d
DualMatrix <piet.goris@gmail.com>
2018-09-11 16:31:08 +0200

    Allow terrain explosing to be circular

--- scripts/terrain.gd
+++ scripts/terrain.gd
@@ -13,12 +13,12 @@ func get_AABB():
 func is_tile_explodable(id):
 	return [7].has(id)
 
-func explode_around(position, diameter = 5):
+func explode_around(position, radius = 3):
 	print(world_to_map(position))
 	var nearest_grid_id = world_to_map(position)
-	for i in range(nearest_grid_id.x - diameter/2, nearest_grid_id.x + diameter/2 + 1):
-		for j in range(nearest_grid_id.y - diameter/2 - 1, nearest_grid_id.y + diameter/2):
-			if(is_tile_explodable(get_cell(i,j))):
+	for i in range(nearest_grid_id.x - radius, nearest_grid_id.x + radius + 1):
+		for j in range(nearest_grid_id.y - radius - 1, nearest_grid_id.y + radius):
+			if(position.distance_to(map_to_world(Vector2(i,j))) < radius * 64):
 				set_cell(i,j,-1)
 			
 	
