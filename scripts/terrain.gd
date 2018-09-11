31239fcc782f4601317b738310a0efb1b1e92270
DualMatrix <piet.goris@gmail.com>
2018-09-11 16:11:20 +0200

    Made terrain explosions better, only red_block_1 can explode now

--- scripts/terrain.gd
+++ scripts/terrain.gd
@@ -9,3 +9,16 @@ func get_AABB():
 	rect.size *= cell_size
 	
 	return rect
+
+func is_tile_explodable(id):
+	return [7].has(id)
+
+func explode_around(position, diameter = 5):
+	print(world_to_map(position))
+	var nearest_grid_id = world_to_map(position)
+	for i in range(nearest_grid_id.x - diameter/2, nearest_grid_id.x + diameter/2 + 1):
+		for j in range(nearest_grid_id.y - diameter/2 - 1, nearest_grid_id.y + diameter/2):
+			if(is_tile_explodable(get_cell(i,j))):
+				set_cell(i,j,-1)
+			
+	
